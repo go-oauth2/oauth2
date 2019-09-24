7d41418071a080297168df2325f8641eea212fcc
Doug McInnes <doug@dougmcinnes.com>
2019-09-24 16:17:14 -0700

    add comments to expiration config

--- manage/config.go
+++ manage/config.go
@@ -4,9 +4,9 @@ import "time"
 
 // Config authorization configuration parameters
 type Config struct {
-	// access token expiration time
+	// access token expiration time, 0 means it doesn't expire
 	AccessTokenExp time.Duration
-	// refresh token expiration time
+	// refresh token expiration time, 0 means it doesn't expire
 	RefreshTokenExp time.Duration
 	// whether to generate the refreshing token
 	IsGenerateRefresh bool
@@ -14,9 +14,9 @@ type Config struct {
 
 // RefreshingConfig refreshing token config
 type RefreshingConfig struct {
-	// access token expiration time
+	// access token expiration time, 0 means it doesn't expire
 	AccessTokenExp time.Duration
-	// refresh token expiration time
+	// refresh token expiration time, 0 means it doesn't expire
 	RefreshTokenExp time.Duration
 	// whether to generate the refreshing token
 	IsGenerateRefresh bool
